ebecaf96616c745b4a8a10a1b667ba5f24b62862
Deepanki Jain <deepankijain@gmail.com>
2021-06-08 20:20:58 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import PostsContainer from './components/PostsContainer'
+import Topbar from './components/Topbar'
+
+function App() {
+  return (
+    <div>
+      <Topbar />
+      <PostsContainer />
+    </div>
+  )
+}
+
+export default App
